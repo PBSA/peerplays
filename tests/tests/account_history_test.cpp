86b7a9a007c83413cef83f5ac97f4ce7d6b4e727
gladcow <s.gladkov@pbsa.info>
2019-08-26 13:57:38 +0300

    fix copyrigth messages order

--- tests/tests/account_history_test.cpp
+++ tests/tests/account_history_test.cpp
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2019 PBSA, and contributors.
+ */
+/*
  * Copyright (c) 2015 Cryptonomex, Inc., and contributors.
  *
  * The MIT License
@@ -20,9 +23,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- */
-/*
- * Copyright (c) 2019 PBSA, and contributors.
  */
 #include <boost/test/unit_test.hpp>
 
