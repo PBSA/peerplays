f6423d0b10060c470166464efb853a875a1e10ee
satyakoneru <satyakoneru.iiith@gmail.com>
2019-08-28 13:53:41 +0000

    GRPH-4-CliWallet_crash_ctrlD

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -226,7 +226,7 @@ namespace detail {
 
       void new_connection( const fc::http::websocket_connection_ptr& c )
       {
-         auto wsc = std::make_shared<fc::rpc::websocket_api_connection>(*c);
+         auto wsc = std::make_shared<fc::rpc::websocket_api_connection>(c);
          auto login = std::make_shared<graphene::app::login_api>( std::ref(*_self) );
          login->enable_api("database_api");
 
