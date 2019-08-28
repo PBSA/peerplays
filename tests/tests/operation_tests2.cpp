fd35f34ed0f403a067aa07da1743c409b1f840b1
Miha Čančula <miha@noughmad.eu>
2019-08-28 15:37:52 +0200

    Make all the tests compile

--- tests/tests/operation_tests2.cpp
+++ tests/tests/operation_tests2.cpp
@@ -1316,7 +1316,6 @@ BOOST_AUTO_TEST_CASE(zero_second_vbo)
          create_op.owner = alice_id;
          create_op.amount = asset(500);
          create_op.policy = pinit;
-         create_op.balance_type = vesting_balance_type::unspecified;
 
          signed_transaction create_tx;
          create_tx.operations.push_back( create_op );
@@ -1400,7 +1399,6 @@ BOOST_AUTO_TEST_CASE( vbo_withdraw_different )
          create_op.owner = alice_id;
          create_op.amount = asset(100, stuff_id);
          create_op.policy = pinit;
-         create_op.balance_type = vesting_balance_type::unspecified;
 
          signed_transaction create_tx;
          create_tx.operations.push_back( create_op );
