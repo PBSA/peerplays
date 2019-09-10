00a38c521e6ed8dcfb3237f6eadb7b2fda2e4313
Miha Čančula <miha@noughmad.eu>
2019-09-10 10:26:05 +0200

    Update the FC submodule with the changes for GRPH-4

--- tests/cli/main.cpp
+++ tests/cli/main.cpp
@@ -210,7 +210,7 @@ public:
       wallet_data.ws_password = "";
       websocket_connection  = websocket_client.connect( wallet_data.ws_server );
 
-      api_connection = std::make_shared<fc::rpc::websocket_api_connection>(*websocket_connection);
+      api_connection = std::make_shared<fc::rpc::websocket_api_connection>(websocket_connection);
 
       remote_login_api = api_connection->get_remote_api< graphene::app::login_api >(1);
       BOOST_CHECK(remote_login_api->login( wallet_data.ws_user, wallet_data.ws_password ) );
